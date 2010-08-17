54832e0d01ff86a23435910bbda0676a32bdf317
Tim Horton <hortont424@gmail.com>
2010-08-16 22:50:55 -0700

    Don't load invalid files, print debug messages, check file extension

--- src/main.py
+++ src/main.py
@@ -24,9 +24,22 @@ cal.add('version', '2.0')
 
 for root, dirs, files in os.walk("data"):
     for file in files:
-        prefix = "({0}) ".format(os.path.splitext(os.path.basename(file))[0])
+        fileBase, fileExtension = os.path.splitext(os.path.basename(file))
 
-        for jsevent in json.loads(readFile(os.path.join(root, file))):
+        if not fileExtension.lower().endswith("cal"):
+            continue
+
+        prefix = "({0}) ".format(fileBase.replace("-", " "))
+
+        print "Loading {0} ...".format(file),
+
+        try:
+            jsevents = json.loads(readFile(os.path.join(root, file)))
+        except Exception as e:
+            print "failed (invalid file: {0})!".format(str(e))
+            continue
+
+        for jsevent in jsevents:
             icsevt = icalendar.Event()
             evt = Event(jsevent)
 
@@ -45,5 +58,10 @@ for root, dirs, files in os.walk("data"):
             icsevt.add('transp', "TRANSPARENT")
             icsevt['uid'] = uuid.uuid4().hex
             cal.add_component(icsevt)
+
+        if len(jsevents) == 1:
+            print "got {0} event!".format(len(jsevents))
+        else:
+            print "got {0} events!".format(len(jsevents))
 
 startServer(cal.as_string())
